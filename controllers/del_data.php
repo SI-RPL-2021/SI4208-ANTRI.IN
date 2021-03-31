cad08a23449821ab9e3a023cd642fcef09b4e23e
zeeniye <bluemul07@gmail.com>
2021-03-31 10:13:49 +0800

    edit delete function for doctor list ref #15

---
+++ controllers/del_data.php
@@ -0,0 +1,24 @@
+<?php
+require 'db_connect.php';
+function del_data_dokter($key_item){
+    global $conn;
+    query("DELETE FROM `List Dokter` WHERE `List Dokter`.`id_dokter` = '$key_item'");
+    $eff_rw = mysqli_affected_rows($conn);
+    
+    return $eff_rw;
+    // if($eff_rw>0) {
+    //     echo "
+    //     <script>
+    //         alert('Event deleted!!');
+    //         document.location.href = 'home_event.php';
+    //     </script>
+    // ";
+    // } else {
+    //     echo "
+    //     <script>
+    //         alert('Event not deleted');
+    //         document.location.href = 'home_event.php';
+    //     </script>
+    // ";
+    // }
+}
