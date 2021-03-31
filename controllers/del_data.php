dc1dea2e8211f5e322c1f93c09718e8e73e7882a
zeeniye <bluemul07@gmail.com>
2021-03-31 10:17:16 +0800

    add function to view/select data (globally for every table in the db) ref #17

--- controllers/del_data.php
+++ controllers/del_data.php
@@ -1,8 +1,10 @@
 <?php
 require 'db_connect.php';
+require 'select_data.php';
+
 function del_data_dokter($key_item){
     global $conn;
-    query("DELETE FROM `List Dokter` WHERE `List Dokter`.`id_dokter` = '$key_item'");
+    view_data("DELETE FROM `List Dokter` WHERE `List Dokter`.`id_dokter` = '$key_item'");
     $eff_rw = mysqli_affected_rows($conn);
     
     return $eff_rw;
