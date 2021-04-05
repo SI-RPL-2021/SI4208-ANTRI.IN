c83f076f7d8960c1dc2a49d55a6827d1afa0ae44
zeeniye <bluemul07@gmail.com>
2021-04-05 10:52:09 +0800

    edit database direktori

--- sql_import.bat
+++ sql_import.bat
@@ -4,4 +4,4 @@ cd "C:\xampp\mysql\bin"
 
 mysql -u root -e "DROP DATABASE IF EXISTS antriin; CREATE DATABASE IF NOT EXISTS antriin;"
 
-mysql -u root antriin < "../storages/database/antriin.sql"
+mysql -u root antriin < "/storages/database/antriin.sql"
