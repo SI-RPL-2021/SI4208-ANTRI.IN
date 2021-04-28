2b9a91ad49607b53eafd3db7daf5a9ffd6257a49
zeeniye <bluemul07@gmail.com>
2021-04-28 16:42:00 +0800

    [backend] admin can update poliklinik data ref #21

--- controllers/edit_data.php
+++ controllers/edit_data.php
@@ -20,6 +20,12 @@ if(isset($_POST["edit_hospital"])){
     header("Location: ../views/admins/adminRumahSakit.php");
 }
 
+// ubah poliklinik
+if(isset($_POST["edit_poli"])){
+    $_SESSION['eff_edit'] = update_poli($_POST, $_SESSION['id_no']);
+    header("Location: ../views/admins/adminPoliklinik.php?id_rs=".$_POST['id_rs_hid']);
+}
+
 function update_dokter($data, $id_dok){
     global $conn;
     // $rs_row = view_data("SELECT * FROM `list_dokter` WHERE id_dokter='$id_dok'")[0];
@@ -83,6 +89,32 @@ function update_hospital($data, $id_rs){
 
     $update_stmt = mysqli_prepare($conn, $query);
     mysqli_stmt_bind_param($update_stmt,'sssi', $nama, $alamat, $telepon, $id_rs);
+
+    if(!empty($update_stmt)){
+        mysqli_stmt_execute($update_stmt);
+    }else{var_dump('gagal');}
+
+    // mysqli_query($conn, $query);
+    $eff_rw = mysqli_affected_rows($conn);
+    mysqli_close($conn);
+    
+    return $eff_rw;
+}
+
+function update_poli($data, $id_poli){
+    global $conn;
+    // $rs_row = view_data("SELECT * FROM `list_dokter` WHERE id_dokter='$id_dok'")[0];
+    $update_stmt = '';
+    $query = '';
+
+    $nama = $data['namaPoli'];
+    $jadwal = $data['jadwalPoli'];
+    $id_dok = $data['drName'];
+    $id_rs = $data['id_rs_hid'];
+    $query = "UPDATE `poliklinik` SET `nama_poli`=?, `jadwal_buka`=?, `id_dokter`=?, `id_rs`=? WHERE `id_poli`=?";
+
+    $update_stmt = mysqli_prepare($conn, $query);
+    mysqli_stmt_bind_param($update_stmt,'ssiii', $nama, $jadwal, $id_dok, $id_rs, $id_poli);
 
     if(!empty($update_stmt)){
         mysqli_stmt_execute($update_stmt);
--- views/admins/editPoliklinik.php
+++ views/admins/editPoliklinik.php
@@ -1,3 +1,15 @@
+<?php
+require_once '../../controllers/select_data.php';
+$dr_res = view_data("SELECT * FROM list_dokter");
+
+if (isset($_GET['id_poli'])) {
+    $id_num = $_GET['id_poli'];
+    $result = view_data("SELECT * FROM poliklinik WHERE id_poli=$id_num")[0];
+    $_SESSION['id_no'] = $id_num;
+}
+
+$sltcd = '';
+?>
 <!doctype html>
 <html lang="en">
 
@@ -7,8 +19,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <title>Admin | Edit Data Poliklinik</title>
 </head>
@@ -16,65 +27,76 @@
 <body style="background-color: rgb(181, 240, 181);">
     <!--Navbar-->
     <div class="container">
-        <font color=white>
             <div class="d-flex justify-content-between">
-                <nav class="navbar navbar-expand-lg navbar-light bg-dark fixed-top">
+                <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
                     &nbsp;&nbsp;&nbsp;
-                    <img src="logo.png" width="60" alt="">
+                    <a class="navbar-brand" href="">
+                        <img src="../../storages/gambar/logo.png" width="60" alt="ANTRI.IN">
+                    </a>
                     <div class="collapse navbar-collapse nav justify-content-center" id="navbarSupportedContent">
                         <ul class="navbar-nav mr-auto">
                             <li class="nav-item active">
-                                <h4>Edit Data Poliklinik</h4>
+                                <a class="nav-link active" aria-current="page" href="">
+                                    <h4>Edit Data Poliklinik</h4>
+                                </a>
                             </li>
                         </ul>
                     </div>
-                    <button type="button" class="btn btn-danger">Cancel</button>
+                    <a href="adminPoliklinik.php?id_rs=<?= $result['id_rs'] ?>" type="button" class="btn btn-danger">Cancel</a>
                     &nbsp;&nbsp;
-                    <button type="button" class="btn btn-primary">Simpan</button>
-                    &nbsp;&nbsp;
+                    <!--Form-->
+                    <form action="../../controllers/edit_data.php" method="POST">
+                        <input type="submit" class="btn btn-primary" value="Simpan" name="edit_poli"></input>
+                        <input type="hidden" name="id_rs_hid" value="<?= $result['id_rs'] ?>"></input>
+                        &nbsp;&nbsp;
                 </nav>
-        </font>
     </div>
     </div>
     <!--Navbar-->
     <br><br><br><br>
     <div class="container" style="background-color: white; border-radius: 7px;">
         <center>
-            <img src="logo2.png" width="500px">
+            <img src="../../storages/gambar/logo2.png" width="500px">
         </center>
         <!--Grid-->
         <div class="container">
             <div class="row">
                 <div class="col-sm">
                     <center>
-                        <img src="poli.png" width="220">
+                        <img src="../../storages/gambar/poli.png" width="220">
                     </center>
                 </div>
                 <div class="col-sm">
-                    <!--Form-->
-                    <form action="">
-
-                        <div class="mb-3">
-                            <label for="exampleInputNama" class="form-label">Nama Poliklinik</label>
-                            <input type="nama" class="form-control" id="exampleInputNama"
-                                aria-describedby="namaHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputDokter" class="form-label">Dokter</label>
-                            <input type="dokter" class="form-control" id="exampleInputDokter"
-                                aria-describedby="dokterHelp">
-                        </div>
-                        <div class="mb-3">
-                            <label for="exampleInputJadwal" class="form-label">Jadwal Praktik</label>
-                            <input type="jadwal" class="form-control" id="exampleInputJadwal"
-                                aria-describedby="jadwalHelp">
-                        </div>
+                    <div class="mb-3">
+                        <label for="exampleInputNama" class="form-label">Nama Poliklinik</label>
+                        <input type="nama" class="form-control" id="exampleInputNama" aria-describedby="namaHelp" name="namaPoli" value="<?= $result['nama_poli']?>">
+                    </div>
+                    <div class="mb-3">
+                        <label for="cats" class="form-label">Dokter</label>
+                        <select name="drName" id="cats" class="form-control">
+                        <?php
+                        foreach ($dr_res as $dr) {
+                            if($dr['id_dokter'] == $result['id_dokter']){
+                                $sltcd='selected';
+                            }else{$sltcd='';}
+                        ?>
+                            <option value="<?= $dr['id_dokter'] ?>" <?= $sltcd ?> ><?= $dr['nama_dokter'] ?></option>
+                            <!-- <input type="hidden" name="id_dokter_hid" value="<?= $dr['id_dokter'] ?>" class="form-control"></input> -->
+                        <?php
+                        }
+                        ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleInputJadwal" class="form-label">Jadwal Praktik</label>
+                        <input type="jadwal" class="form-control" id="exampleInputJadwal" aria-describedby="jadwalHelp" name="jadwalPoli" value="<?= $result['jadwal_buka']?>">
+                    </div>
 
                     </form>
                     <!--Form-->
                 </div>
                 <div class="col-sm">
-                
+
                 </div>
             </div>
         </div>
@@ -86,9 +108,8 @@
     <!-- Optional JavaScript; choose one of the two! -->
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf"
-        crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    </script>
 
     <!-- Option 2: Separate Popper and Bootstrap JS -->
     <!--
